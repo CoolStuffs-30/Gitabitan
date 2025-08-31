f6781c557ff99ba10bf2cdbad270fc2c3c204579
Soujit Paul <gatestudiez@gmail.com>
2025-08-31 11:11:15 +0530

    s

--- js/fab.js
+++ js/fab.js
@@ -16,6 +16,6 @@
     });
 
     document.fonts.ready.then(() => {
-      const callIcon = document.querySelector('.material-symbols-outlined.fab-icon');
+      const callIcon = document.querySelector('.material-symbols-outlined');
       if (callIcon) callIcon.classList.add('font-loaded');
     });
